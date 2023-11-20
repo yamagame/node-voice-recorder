e584cf5b77d073096e0d2c4ef76ce94e089902ea
yamagame <yamagame@mac.com>
2023-11-20 23:53:44 +0900

    向き先修正

--- scripts/connect-dora-engine.sh
+++ scripts/connect-dora-engine.sh
@@ -12,5 +12,5 @@ echo connecting $DORA_IP from $HOST_IP
 curl -X POST --data "{\"host\":\"${HOST_IP}\", \"port\":\"3093\"}" --header "content-type:application/json" http://$DORA_IP:3090/reazon/config
 export VOICE_RECORDER_SERVER_PORT=3093
 export VOICE_RECORDER_ENERGY_POS=5
-export AGENT_HOST=http://$HOST_IP:3090
+export AGENT_HOST=http://$DORA_IP:3090
 yarn start
